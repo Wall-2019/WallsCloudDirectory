9499b1d2bf343b69f6745a55a454bcd15107be4f
kazumakato-walls <107688753+kazumakato-walls@users.noreply.github.com>
2024-08-28 17:58:44 +0900

    push

--- app/components/Welcome/Welcome.tsx
+++ app/components/Welcome/Welcome.tsx
@@ -5,8 +5,6 @@ import Link from "next/link";
 export function Welcome() {
 
   const test = () => {
-    const test = process.env.NEXT_PUBLIC_URL;
-    const test2 = process.env.CUSTOMCONNSTR_NEXT_PUBLIC_BACKEND;
     console.log("Backend URL:", process.env.NEXT_PUBLIC_URL);
     console.log("Backend URL:", process.env.NEXT_PUBLIC_AUTH_SECRET);
   }
