fb51d77db8585263f4326e3f0c44a1976bd333b7
junsato-walls <j.sato@walls-inc.com>
2024-09-02 15:22:05 +0900

    20240902

--- app/components/Options/SystemInfo.tsx
+++ app/components/Options/SystemInfo.tsx
@@ -5,7 +5,16 @@ import axios from 'axios';
 
 export function SystemInfo() {
   const backendUrl = process.env.NEXT_PUBLIC_URL as string;
+  const { data: session, status } = useSession();
+  const [backendVer, setBackendVer] = useState('')
+  const [databaseVer, setDatabaseVer] = useState('')
+
   const now = new Date();
+  const config = {
+    headers: {
+      Authorization: 'Bearer ' + session?.user?.accessToken
+    },
+  };
 
   const [time, setTime] = useState(new Date());
   useEffect(() => {
@@ -21,18 +30,33 @@ export function SystemInfo() {
     const min = time.getMinutes();
     const sec = time.getSeconds();
 
+    const getVersion = async () => {
+      try {
+        const res = await axios.get(`${backendUrl}/systeminfo/get_systeminfo`, config);
+        console.log(res.data)
+        setBackendVer(res.data.backend_version)
+        setDatabaseVer(res.data.database_version)
+      } catch (err) {
+        console.error("err:", err);
+      }
+    };
+    useEffect(() => {
+      if (status === 'authenticated') {
+        getVersion();
+      }
+    }, []);
+
   return (
     <Container size={9000} my={40}>
       <Title>システム情報</Title>
       
       <Paper withBorder shadow="md" p={10} mt={10} radius="md">            
             <p>システムバージョン</p>
-            <p>　-フロントエンド:1.0</p>
-            <p>　-バックエンド:</p>
-            <p>　-データベース:</p>
+            <p>　-フロントエンド:ver.1.0</p>
+            <p>　-バックエンド:{backendVer}</p>
+            <p>　-データベース:{databaseVer}</p>
             <p>バックエンドURL:{backendUrl}</p>
             <p>システム時間:{hour}時{min}分{sec}秒</p>
-            
       </Paper>
     </Container>
   );
